991fb1191a2680bc0827a85407792e95cc36356f
Saksham Mittal <gotlouemail@gmail.com>
2022-04-02 19:15:26 +0530

    Add attempt counter and disable further play if won

--- script.js
+++ script.js
@@ -2,13 +2,33 @@ let answer = "hello";
 let form = document.querySelector('form');
 
 let input = document.querySelector('input');
+let name = document.querySelector('#result');
+let win = document.querySelector('#win');
+let attempt = document.querySelector('#attempt');
 
-function hello(event) {
+let exit = false;
+let tries = 0;
+
+function checkAnswer(event) {
 	event.preventDefault();
-	let name = document.querySelector('#result');
-	if (input.value.length == 5) {
-		name.innerHTML = `${input.value}`;
-	} else {
-		name.innerHTML = "";
+	if (!exit) {
+		if (input.value.length == 5) {
+			tries++;
+			name.innerHTML = `${input.value}`;
+			if (input.value == answer) {
+				win.innerHTML = "You won!";
+				exit = true;
+			} else {
+				win.innerHTML = "Not quite right!";
+			}
+			if (tries == 1) {
+				attempt.innerHTML = tries + " attempt taken";
+			} else {
+				attempt.innerHTML = tries + " attempts taken";
+			}
+
+		} else {
+			name.innerHTML = "";
+		}
 	}
 }
